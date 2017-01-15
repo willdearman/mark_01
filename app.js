51a5d3de9359c835533b2ae359bd25bd78b3e660
Will Dearman <will@willdearman.com>
2017-01-14 18:24:26 -0600

    Working version before configuring Drone for CI

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var googleFinance = require('google-finance');
+//var yahooFinance = require('yahoo-finance');
 
-// Create bot and add dialogs
-var connector = new builder.ChatConnector({
-    //appId: "YourAppId",
-    //appPassword: "YourAppSecret"
-});
-var bot = new builder.UniversalBot(connector);  
-bot.dialog('/', function (session) {
-    session.send('Hello World');
-});
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
-server.post('/api/messages', connector.listen());
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var SYMBOL = 'NASDAQ:AAPL';
+var FROM = '2014-01-01';
+var TO = '2014-12-31';
+var util = require('util');
+
+
+bot.dialog('/', function (session) {
+    googleFinance.historical({
+  symbol: SYMBOL,
+  from: FROM,
+  to: TO
+}).then(function (quotes) {
+  console.log(util.format(
+    '=== %s (%d) ===',
+    SYMBOL,
+    quotes.length
+  ).cyan);
+  if (quotes[0]) {
+  	session.send('Closing price is %s', JSON.stringify(quotes[0].close, null, 2));
+    //console.log(
+    //  '%s\n...\n%s',
+    //  JSON.stringify(quotes[0], null, 2),
+    //  JSON.stringify(quotes[quotes.length - 1], null, 2)
+    //);
+  } else {
+    console.log('N/A');
+  }
+});
+    //session.send();
+    session.send("Response is complete");
+});
+
+
+
+
+
