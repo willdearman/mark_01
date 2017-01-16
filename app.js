5d504fdfc07ec8a7ed8582efbce00ab1f4b95eff
Will Dearman <will@willdearman.com>
2017-01-16 15:45:04 -0600

    Now gets company news

--- app.js
+++ app.js
@@ -33,12 +33,15 @@ server.post('/api/messages', connector.listen());
 bot.dialog('/', [
     function (session) {
         session.send(prompts.welcomeMessage);
-        session.send("Value is coming");
         session.beginDialog('/getTickerPrice');
     },function (session) {
-      console.log(session.privateConversationData['entity1'])
-        session.send("The price is %(close)s for %(symbol)s", session.privateConversationData['entity1']);
-    },function (session) {
+        console.log(session.privateConversationData['entity1quotes'])
+        session.send("The closing price was %(close)s for %(symbol)s on %(date)s", session.privateConversationData['entity1quotes']);
+        session.beginDialog('/getTickerNews');
+    },function (session, next) {
+        session.send("Recent company news includes");
+        console.log(session.privateConversationData['entity1news'])
+        session.send("%(title)s", session.privateConversationData['entity1news']);
         session.endConversation("Goodbye!");
     }
 ]);
@@ -51,13 +54,25 @@ bot.dialog('/getTickerPrice', [
         var SYMBOL = 'NASDAQ:AAPL';
         var FROM = '2014-01-01';
         var TO = '2014-12-31';
-        //builder.Prompts.text(session, 'Hi! What is your name?');
-        session.send("Obtaining value");
+
         googleFinance.historical({symbol: SYMBOL, from: FROM, to: TO}).then(function (quotes) 
     {
           console.log(quotes[0])
-          session.privateConversationData['entity1'] = quotes[0]
-          session.send("I have a value")
+          session.privateConversationData['entity1quotes'] = quotes[0]
+          session.endDialog();
+    })
+  }
+]);
+
+bot.dialog('/getTickerNews', [
+    function (session, news)  {
+        var SYMBOL = 'NASDAQ:AAPL';
+        //builder.Prompts.text(session, 'Hi! What is your name?');
+        session.send("Obtaining news value");
+        googleFinance.companyNews({symbol: SYMBOL}).then(function (news) 
+    {
+          console.log(news[0])
+          session.privateConversationData['entity1news'] = news[0]
           session.endDialog();
     })
   }
