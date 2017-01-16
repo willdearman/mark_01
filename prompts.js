277b4af0a8bb6e3a35e268ec932711d4d35d774a
Will Dearman <will@willdearman.com>
2017-01-16 12:09:20 -0600

    Corrected typo

---
+++ prompts.js
@@ -0,0 +1,12 @@
+module.exports = {
+    welcomeMessage:     "Hello! My name is Mark and I am the Bot from MarketBot.io. I'm not quite ready for production yet, but thanks for helping test me." +
+                        "\n  * Try asking me what the price of MSFT was on January 1, 2015." +
+                        "\n\nOnce you ask a question about a company you can ask followup questions about the same company.",
+// Begin comments from old bot:
+    helpMessage: "I can answer questions about a handfull securities. Here are some of the things you can ask:\n\n",
+    companyUnknown: "I don't know '%(company)s'. Pick a company from the list or say 'nevermind'.",
+    companyMissing: "I can answer that first tell me what company you're interested in:",
+    answerWebsite: "The website for %(company)s is %(value)s.",
+    answerPrice: "The price per share for %(company)s is %(value)s.",
+    comparePrice: "There is %(entityOne)s and %(entityTwo)s one of them is valued at %(value)s."
+};
